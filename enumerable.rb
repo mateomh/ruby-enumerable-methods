4ce3a4c6e943dea304a908b44f5f51c09173c656
Ramin Mammadzada <remki-design@Hotmail.com>
2020-08-10 23:42:32 +0300

    Add #my_each_with_index.

--- enumerable.rb
+++ enumerable.rb
@@ -7,11 +7,29 @@ module Enumerable
       yield(self[index])
     end
   end
+
+  def my_each_with_index(&block)
+    # your code here
+    #
+    self.length().times do |index|
+      yield(self[index], index)
+    end
+  end
 end
 
 my_array = %w[jerry tom dine]
 
 
+puts "Test #my_each method"
 my_array.my_each do |elem|
   puts elem
 end
+puts "- - - -"
+
+puts "Test #my_each_with_index method"
+my_array.my_each_with_index do |elem, index|
+  puts "Element: " + elem + " | " + "Index: " + index.to_s
+end
+puts "- - - -"
+
+
